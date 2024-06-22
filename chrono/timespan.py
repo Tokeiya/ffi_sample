b9ea9769c6c1b41c47664f9eb5ae248b9a4dbeab
net_s <netseed@seedcreate.onmicrosoft.com>
2024-06-22 23:50:32 +0900

    Add TimeSpan class and update main.py
    A new class, TimeSpan, is introduced which includes several methods, yet to be implemented. Major changes to main.py were also made, adding ctypes usage and altering the main function to interact with a dynamic link library.

---
+++ chrono/timespan.py
@@ -0,0 +1,65 @@
+class TimeSpan:
+    _nano: int
+
+    def __init__(self, value: int):
+        raise NotImplementedError()
+
+    def total_nano_seconds(self) -> float:
+        raise NotImplementedError()
+
+    def total_micro_seconds(self) -> float:
+        raise NotImplementedError()
+
+    def total_milli_seconds(self) -> float:
+        raise NotImplementedError()
+
+    def total_seconds(self) -> float:
+        raise NotImplementedError()
+
+    def total_minutes(self) -> float:
+        raise NotImplementedError()
+
+    def total_hours(self) -> float:
+        raise NotImplementedError()
+
+    def nano_seconds(self) -> int:
+        raise NotImplementedError()
+
+    def micro_seconds(self) -> int:
+        raise NotImplementedError()
+
+    def milli_seconds(self) -> int:
+        raise NotImplementedError()
+
+    def seconds(self) -> int:
+        raise NotImplementedError()
+
+    def minutes(self) -> int:
+        raise NotImplementedError()
+
+    def hours(self) -> int:
+        raise NotImplementedError()
+
+    def __add__(self, other: 'TimeSpan') -> 'TimeSpan':
+        raise NotImplementedError()
+
+    def __sub__(self, other: 'TimeSpan') -> 'TimeSpan':
+        raise NotImplementedError()
+
+    def __eq__(self, other: 'TimeSpan') -> bool:
+        raise NotImplementedError()
+
+    def __ne__(self, other: 'TimeSpan') -> bool:
+        raise NotImplementedError()
+
+    def __lt__(self, other: 'TimeSpan') -> bool:
+        raise NotImplementedError()
+
+    def __le__(self, other: 'TimeSpan') -> bool:
+        raise NotImplementedError()
+
+    def __gt__(self, other: 'TimeSpan') -> bool:
+        raise NotImplementedError()
+
+    def __ge__(self, other: 'TimeSpan') -> bool:
+        raise NotImplementedError()
--- python/main.py
+++ python/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("hell python")
+import ctypes
+import use_pyo3
 
+
+def main():
+    lib = ctypes.CDLL('../artifacts/primiive_ffi.dll')
+    lib.add.argtypes = (ctypes.c_int, ctypes.c_int)
+    lib.add.restype = ctypes.c_int
+
+    result = lib.add(3, 4)
+    print(f"Result of add(3, 4): {result}")
+
+    print(use_pyo3.sub(20, 10))
+    print(use_pyo3.answer())
+    
 
 if __name__ == "__main__":
     main()
