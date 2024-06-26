f16d8bf528c0bdd552d83354f3feffa186c5811c
net_s <netseed@seedcreate.onmicrosoft.com>
2024-06-26 21:38:53 +0900

    Add measurement and run_autofit_benchmark functions
    Introduced the measurement function to calculate iterations required to meet a time threshold. Also added a placeholder for the run_autofit_benchmark function to handle benchmarks with automatic iteration adjustments based on the target execution time.

--- python_packages/tiny_bench/runnner.py
+++ python_packages/tiny_bench/runnner.py
@@ -1,9 +1,10 @@
 import gc
 
-from python_packages.chrono import TimeSpan
+from python_packages.chrono import TimeSpan, from_seconds, from_milli_seconds
 from python_packages.chrono import MonotonicChronograph
 
 from typing import Callable, Any, List, Tuple
+import math
 
 
 def run_benchmark(bench: Callable[[], Any], category: str, iteration: int) -> List[Tuple[str, 'TimeSpan']]:
@@ -22,3 +23,53 @@ def run_benchmark(bench: Callable[[], Any], category: str, iteration: int) -> Li
         result.append((category, ret.elapsed))
 
     return result
+
+
+def measurement(target: Callable[[int], Any], threshold: 'TimeSpan') -> int:
+    pre_threshold = from_milli_seconds(100)
+    iteration: int = 1
+    chrono = MonotonicChronograph()
+    elapsed = TimeSpan(0)
+    flg = False
+
+    for i in range(100):
+        chrono.restart()
+        target(iteration)
+        elapsed = chrono.stop().elapsed
+
+        print(f'iteration:{iteration} elapsed:{elapsed.total_milli_seconds()} ms')
+        if elapsed >= pre_threshold:
+            flg = True
+            break
+
+        iteration = math.ceil(iteration * 1.5)
+
+    if not flg:
+        raise RuntimeWarning('No solution')
+
+    flg = False
+    per = elapsed / iteration
+    iteration = int(threshold / per)
+
+    inclement = int(iteration / 100) + 1
+
+    for i in range(100):
+        chrono.restart()
+        target(iteration)
+        elapsed = chrono.stop().elapsed
+        print(f'iteration:{iteration} elapsed:{elapsed.total_milli_seconds()} ms')
+        if elapsed >= threshold:
+            flg = True
+            break
+
+        inclement += inclement
+
+    if not flg:
+        raise RuntimeWarning('No solution')
+
+    return iteration
+
+
+def run_autofit_benchmark(target: Callable[[int], Any], threshold: 'TimeSpan', env: str, iteration: int) -> List[
+    Tuple[str, str, int, 'TimeSpan']]:
+    pass
